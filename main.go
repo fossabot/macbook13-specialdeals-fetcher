a23f709a476c8c5d64d7dcea2973f857c38dee4f
Yoshiyuki HINO <yhinoz@gmail.com>
2018-04-18 00:20:49 +0900

    init

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type Product struct {
+	ID    string   `json:"id"`
+	Name  string   `json:"name"`
+	Specs []string `json:"specs"`
+	Price string   `json:"price"`
+}
+
+func main() {
+	doc, err := goquery.NewDocument("https://www.apple.com/jp/shop/browse/home/specialdeals/mac/macbook_pro/13")
+	if err != nil {
+		panic(err)
+	}
+
+	var products []*Product
+	doc.Find("tr.product").Each(func(i int, s *goquery.Selection) {
+		specsSelection := s.Find("td.specs")
+
+		productID := specsSelection.Find("h3 > a").AttrOr("data-relatedlink", "UNKNOWN")
+
+		productName := specsSelection.Find("h3").Text()
+		productName = strings.TrimSpace(productName)
+
+		spec := specsSelection.Text()
+		spec = strings.Replace(spec, productName, "", -1)
+
+		var specs []string
+		for _, d := range strings.Split(spec, "\n") {
+			d = strings.TrimSpace(d)
+			if d == "" {
+				continue
+			}
+
+			specs = append(specs, d)
+		}
+
+		price := s.Find("span[itemprop=price]").Text()
+		price = strings.TrimSpace(price)
+
+		products = append(products, &Product{
+			ID:    productID,
+			Name:  productName,
+			Specs: specs,
+			Price: price,
+		})
+	})
+
+	jsonBytes, err := json.Marshal(products)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", jsonBytes)
+}
