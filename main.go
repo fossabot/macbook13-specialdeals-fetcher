6fd1ec73ed3607b6129c3db02073675ab40d821f
Yoshiyuki HINO <yhinoz@gmail.com>
2018-04-29 00:16:54 +0900

    change json column "specs" to "release", "processor", "memory" and "storage"

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 	"regexp"
 	"runtime"
 	"strings"
@@ -13,81 +14,123 @@ import (
 )
 
 type Product struct {
-	ID       string   `json:"id"`
-	Name     string   `json:"name"`
-	Specs    []string `json:"specs"`
-	Keyboard string   `json:"keyboard"`
-	Price    string   `json:"price"`
-	URL      string   `json:"url"`
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Release   string `json:"release"`
+	Processor string `json:"processor"`
+	Memory    string `json:"memory"`
+	Storage   string `json:"storage"`
+	Keyboard  string `json:"keyboard"`
+	Price     string `json:"price"`
+	URL       string `json:"url"`
 }
 
-func fetchProducts(locale string) ([]*Product, error) {
+type ProductParser struct {
+	Document *goquery.Document
+}
+
+func (p *Product) LoadFromURL(url string) error {
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return err
+	}
+
+	parser := NewProductParser(doc)
+
+	p.ID = parser.GetProductID()
+	p.Name = parser.GetProductName()
+	p.Release = parser.GetRelease()
+	p.Processor = parser.GetProcessor()
+	p.Memory = parser.GetMemory()
+	p.Storage = parser.GetStorage()
+	p.Keyboard = parser.GetKeyboard()
+	p.Price = parser.GetPrice()
+
+	p.URL = url
+
+	return nil
+}
+
+func (pp *ProductParser) GetProductID() string {
+	id, ok := pp.Document.Find("input[name=product]").Attr("value")
+	if !ok {
+		return "UNKNOWN"
+	}
+
+	return id
+}
+
+func (pp *ProductParser) GetProductName() string {
+	return strings.TrimSpace(
+		pp.Document.Find("h1[data-autom=productTitle]").Text(),
+	)
+}
+
+func (pp *ProductParser) GetRelease() string {
+	return strings.TrimSpace(
+		pp.Document.Find(".Overview-panel .para-list").Eq(0).Text(),
+	)
+}
+
+func (pp *ProductParser) GetProcessor() string {
+	return strings.TrimSpace(pp.Document.Find("#techSpecsSection .para-list").Eq(0).Text())
+}
+
+func (pp *ProductParser) GetMemory() string {
+	return strings.TrimSpace(pp.Document.Find("#techSpecsSection .para-list").Eq(1).Text())
+}
+
+func (pp *ProductParser) GetStorage() string {
+	s := strings.TrimSpace(pp.Document.Find("#techSpecsSection .para-list").Eq(2).Text())
+	return s[:len(s)-1]
+}
+
+func (pp *ProductParser) GetKeyboard() string {
+	text := pp.Document.Text()
+
+	if regexp.MustCompile(`(?i)JIS.*(キーボード|Key)`).MatchString(text) {
+		return "JIS"
+	}
+
+	if regexp.MustCompile(`(?i)(US|U\.S\.).*(キーボード|Key)`).MatchString(text) {
+		return "US"
+	}
+
+	return "UNKNOWN"
+}
+
+func (pp *ProductParser) GetPrice() string {
+	return strings.TrimSpace(pp.Document.Find(".current_price").Text())
+}
+
+func NewProduct() *Product {
+	return &Product{}
+}
+
+func NewProductParser(doc *goquery.Document) *ProductParser {
+	return &ProductParser{
+		Document: doc,
+	}
+}
+
+func fetchProductURLs(locale string) ([]string, error) {
 	doc, err := goquery.NewDocument("https://www.apple.com/" + locale + "/shop/browse/home/specialdeals/mac/macbook_pro/13")
 	if err != nil {
 		return nil, err
 	}
 
-	var products []*Product
+	var urls []string
 	doc.Find("tr.product").Each(func(i int, s *goquery.Selection) {
-		specsSelection := s.Find("td.specs")
-
-		productID := specsSelection.Find("h3 > a").AttrOr("data-relatedlink", "UNKNOWN")
-		if productID != "UNKNOWN" {
-			productID = strings.Split(productID, "_")[1]
+		url, ok := s.Find("a.button").Attr("href")
+		if !ok {
+			return
 		}
+		url = "https://www.apple.com" + url
 
-		productName := specsSelection.Find("h3").Text()
-		productName = strings.TrimSpace(productName)
-
-		spec := specsSelection.Text()
-		spec = strings.Replace(spec, productName, "", -1)
-
-		var specs []string
-		for _, d := range strings.Split(spec, "\n") {
-			d = strings.TrimSpace(d)
-			if d == "" {
-				continue
-			}
-
-			specs = append(specs, d)
-		}
-
-		price := s.Find("span[itemprop=price]").Text()
-		price = strings.TrimSpace(price)
-
-		url := specsSelection.Find("h3 > a").AttrOr("href", "#")
-		if url != "#" {
-			url = "https://www.apple.com" + url
-		}
-
-		products = append(products, &Product{
-			ID:       productID,
-			Name:     productName,
-			Specs:    specs,
-			Keyboard: "UNKNOWN",
-			Price:    price,
-			URL:      url,
-		})
+		urls = append(urls, url)
 	})
 
-	return products, nil
-}
-
-func (p *Product) fetchDetails() {
-	if p.URL != "#" {
-		detailDoc, err := goquery.NewDocument(p.URL)
-		if err != nil {
-			p.Keyboard = "ERROR"
-		}
-
-		detailFullText := detailDoc.Text()
-		if regexp.MustCompile(`(?i)JIS.*(キーボード|Key)`).MatchString(detailFullText) {
-			p.Keyboard = "JIS"
-		}
-		if regexp.MustCompile(`(?i)(US|U\.S\.).*(キーボード|Key)`).MatchString(detailFullText) {
-			p.Keyboard = "US"
-		}
-	}
+	return urls, nil
 }
 
 func main() {
@@ -98,30 +141,38 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	var wg sync.WaitGroup
 
-	products, err := fetchProducts(*locale)
+	urls, err := fetchProductURLs(*locale)
 	if err != nil {
 		panic(err)
 	}
 
 	// make worker threads
-	queue := make(chan *Product, len(products))
+	var products []*Product
+	queue := make(chan string, len(urls))
 	for i := 0; i < *threads; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 
 			for {
-				product, ok := <-queue
+				url, ok := <-queue
 				if !ok {
 					return
 				}
-				product.fetchDetails()
+
+				product := NewProduct()
+				err := product.LoadFromURL(url)
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+					continue
+				}
+				products = append(products, product)
 			}
 		}()
 	}
 
-	for _, product := range products {
-		queue <- product
+	for _, url := range urls {
+		queue <- url
 	}
 	close(queue)
 	wg.Wait()
